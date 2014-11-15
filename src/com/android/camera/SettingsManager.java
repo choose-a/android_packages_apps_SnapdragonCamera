4dbff06f9e7e9861c74766924c5afb8905a387ef
Wilhelm Fitzpatrick <rafial@cyngn.com>
2014-11-14 16:31:35 -0800

    SnapdragonCamera: dynamically generate available photo resolutions
    Instead of depending on a large matching table, generated the list
    of picture sizes to show to the user directly from the supported
    list returned by the camera. The list is filtered to remove
    uselessly small resolutions on modern devices, to group resolutions
    by aspect ratio, and to filter out fairly similar sizes.
    
    Change-Id: I4824caa62893fc08b1716f4ccc132e5c9c7bf390

--- src/com/android/camera/SettingsManager.java
+++ src/com/android/camera/SettingsManager.java
@@ -316,10 +316,6 @@ public class SettingsManager implements ListMenu.SettingsListener {
         ListPreference picturePref = mPreferenceGroup.findPreference(KEY_PICTURE_SIZE);
         ListPreference videoQualityPref = mPreferenceGroup.findPreference(KEY_VIDEO_QUALITY);
         if (picturePref != null) {
-            picturePref.setEntries(mContext.getResources().getStringArray(
-                    R.array.pref_camera2_picturesize_entries));
-            picturePref.setEntryValues(mContext.getResources().getStringArray(
-                    R.array.pref_camera2_picturesize_entryvalues));
             filterUnsupportedOptions(picturePref, getSupportedPictureSize(
                     getCurrentCameraId()));
         }
@@ -769,13 +765,9 @@ public class SettingsManager implements ListMenu.SettingsListener {
         }
 
         if (pictureSize != null) {
-            if (filterUnsupportedOptions(pictureSize, getSupportedPictureSize(cameraId))) {
-                mFilteredKeys.add(pictureSize.getKey());
-            } else {
-                if (filterSimilarPictureSize(mPreferenceGroup, pictureSize)) {
-                    mFilteredKeys.add(pictureSize.getKey());
-                }
-            }
+            CameraSettings.formatPictureSizes(pictureSize,
+                    getSupportedPictureSizeList(cameraId), mContext);
+            CameraSettings.resetIfInvalid(pictureSize);
         }
 
         if (sceneMode != null) {
@@ -1312,6 +1304,27 @@ private void filterHFROptions() {
         return !hdrOn &&
                 mCharacteristics.get(id).get(CameraCharacteristics.FLASH_INFO_AVAILABLE) &&
                 mValuesMap.get(KEY_FLASH_MODE) != null;
+    }
+
+    private List<Size> getSupportedPictureSizeList(int cameraId) {
+        StreamConfigurationMap map = mCharacteristics.get(cameraId).get(
+                CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+        Size[] sizes = map.getOutputSizes(ImageFormat.JPEG);
+        List<Size> res = new ArrayList<>();
+        if (sizes != null) {
+            for (int i = 0; i < sizes.length; i++) {
+                res.add(sizes[i]);
+            }
+        }
+
+        Size[] highResSizes = map.getHighResolutionOutputSizes(ImageFormat.JPEG);
+        if (highResSizes != null) {
+            for (int i = 0; i < highResSizes.length; i++) {
+                res.add(highResSizes[i]);
+            }
+        }
+
+        return res;
     }
 
     private List<String> getSupportedPictureSize(int cameraId) {
