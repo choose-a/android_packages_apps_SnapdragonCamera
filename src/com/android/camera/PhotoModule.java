dd0021d52c33d7200cb1dbf0b99458303e890f9f
Thecrazyskull <anaskarbila@gmail.com>
2017-01-22 07:50:19 -0600

    Camera: add support for grid mode
    Change-Id: I7557bfd1dcb968bbf7ee17b13e66b8cf182e763e

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -594,6 +594,8 @@ public class PhotoModule
         mLocationManager = new LocationManager(mActivity, this);
         mSensorManager = (SensorManager)(mActivity.getSystemService(Context.SENSOR_SERVICE));
 
+        mUI.getCameraControls().setCameraActivity(mActivity);
+
         brightnessProgressBar = (ProgressBar)mRootView.findViewById(R.id.progress);
         if (brightnessProgressBar instanceof SeekBar) {
             SeekBar seeker = (SeekBar) brightnessProgressBar;
@@ -607,6 +609,8 @@ public class PhotoModule
         brightnessProgressBar.setVisibility(View.INVISIBLE);
         Storage.setSaveSDCard(
             mPreferences.getString(CameraSettings.KEY_CAMERA_SAVEPATH, "0").equals("1"));
+
+        mActivity.showGrid(mPreferences);
     }
 
     private void initializeControlByIntent() {
@@ -2661,6 +2665,7 @@ public class PhotoModule
             s.setListener(null);
         }
         mUI.removeDisplayChangeListener();
+        mActivity.showGrid(mPreferences);
     }
 
     /**
@@ -4760,6 +4765,7 @@ public class PhotoModule
             mHandler.sendEmptyMessage(SET_PHOTO_UI_PARAMS);
         }
         resizeForPreviewAspectRatio();
+        mActivity.showGrid(mPreferences);
     }
 
     @Override
