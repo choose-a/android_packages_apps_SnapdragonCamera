564bbcfab7a8377f486470b5c3227ac700b56815
Erik Castricum <git@cas-online.nl>
2018-05-20 22:46:05 +0200

    Revert "add options to restart preview onPictureTaken"
    This reverts commit c14ede9a3f057ae3e341cc50d8b4db82b27a1166.

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -68,7 +68,6 @@ import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.android.camera.app.CameraApp;
 import com.android.camera.CameraManager.CameraAFCallback;
 import com.android.camera.CameraManager.CameraAFMoveCallback;
 import com.android.camera.CameraManager.CameraPictureCallback;
@@ -272,8 +271,6 @@ public class PhotoModule
             onShutterButtonClick();
         }
     };
-
-    private static Context mApplicationContext;
 
     private class OpenCameraThread extends Thread {
         @Override
@@ -942,8 +939,6 @@ public class PhotoModule
             return;
         }
 
-        mApplicationContext = CameraApp.getContext();
-
         // Initialize location service.
         boolean recordLocation = RecordLocationPreference.get(mPreferences,
                 CameraSettings.KEY_RECORD_LOCATION);
@@ -1410,18 +1405,6 @@ public class PhotoModule
                     && (mCameraState != LONGSHOT)
                     && (mSnapshotMode != CameraInfoWrapper.CAMERA_SUPPORT_MODE_ZSL)
                     && (mReceivedSnapNum == mBurstSnapNum);
-
-            boolean backCameraRestartPreviewOnPictureTaken = 
-                mApplicationContext.getResources().getBoolean(R.bool.back_camera_restart_preview_onPictureTaken);
-            boolean frontCameraRestartPreviewOnPictureTaken = 
-                mApplicationContext.getResources().getBoolean(R.bool.front_camera_restart_preview_onPictureTaken);
-
-            CameraInfo info = CameraHolder.instance().getCameraInfo()[mCameraId];
-            if ((info.facing == CameraInfo.CAMERA_FACING_BACK && backCameraRestartPreviewOnPictureTaken) 
-                || (info.facing == CameraInfo.CAMERA_FACING_FRONT && frontCameraRestartPreviewOnPictureTaken)) {
-                needRestartPreview = true;
-            }
-
             if (needRestartPreview) {
                 setupPreview();
                 if (CameraUtil.FOCUS_MODE_CONTINUOUS_PICTURE.equals(
