a3688bd3a9a0a0ad391469439f6c6325a7d929d3
Erik Castricum <git@cas-online.nl>
2020-02-21 07:56:46 +0100

    build it on Carbon
    Signed-off-by: Erik Castricum <git@cas-online.nl>
    Change-Id: I6a73b6c92f464969ae1fc2c73d15629691a49bfc

--- Android.mk
+++ Android.mk
@@ -24,7 +24,7 @@ LOCAL_AAPT_FLAGS := \
         --version-name "$(version_name_package)" \
         --version-code $(version_code_package) \
 
-LOCAL_PACKAGE_NAME := SnapdragonCamera
+LOCAL_PACKAGE_NAME := Snap
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PRIVATE_PLATFORM_APIS:=true
 
